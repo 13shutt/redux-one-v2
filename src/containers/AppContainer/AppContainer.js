517d8bd8e74737b09e1ce95ccbc62c06da1e5280
13shutt <shutt13a@gmail.com>
2018-10-09 23:06:05 +0300

    styling

--- src/containers/AppContainer/AppContainer.js
+++ src/containers/AppContainer/AppContainer.js
@@ -1,20 +1,47 @@
 import React from 'react'
 import { Route, Switch, Link } from 'react-router-dom'
 
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+
 import JokesList from '../JokesList'
+  
+const styles = {
+  root: {
+    flexGrow: 1,
+  },
+  grow: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginLeft: -12,
+    marginRight: 20,
+  },
+};
 
-import { Header, StyledNav, StyledLink } from '../../components'
-
-const AppContainer = () => {
+const AppContainer = (props) => {
+  const { classes } = props;  
   return (
     <div>
-      <Header>
-        <span>This is my Header!!!</span>
-      </Header>
-      <StyledNav>
-        <Link to="/"><StyledLink>Home</StyledLink></Link>
-        <Link to="/joke"><StyledLink>Fetch one joke</StyledLink></Link>
-      </StyledNav>
+      <div className={classes.root}>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton className={classes.menuButton} color="inherit" aria-label="Menu">
+            ¯\_(ツ)_/¯
+            </IconButton>
+            <Typography variant="h6" color="inherit" className={classes.grow}>
+              My Simple App
+            </Typography>
+            <Button component={Link} color="inherit" to="/">Home</Button>
+            <Button component={Link} color="inherit" to="/joke">Fetch one Joke</Button>
+          </Toolbar>
+        </AppBar>
+      </div>
       <Switch>
         <Route exact path="/" render={() => <div>Simple homepage</div>} />
         <Route path="/joke" component={() => <JokesList />} />
@@ -22,5 +49,9 @@ const AppContainer = () => {
     </div>
   );
 }
- 
-export default AppContainer;
+
+AppContainer.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(AppContainer);
