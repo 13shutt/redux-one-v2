aef23196e5c848437df25927514aac2c83c0b054
13shutt <shutt13a@gmail.com>
2018-10-18 23:22:16 +0300

    view user data - done

--- src/containers/GitUser/GitUser.js
+++ src/containers/GitUser/GitUser.js
@@ -6,8 +6,27 @@ import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import Link from '@material-ui/icons/Link';
+import Note from '@material-ui/icons/Note';
+import People from '@material-ui/icons/People';
+import PeopleOutline from '@material-ui/icons/PeopleOutline';
+import TextField from '@material-ui/core/TextField';
 
 const styles = theme => ({
+  marginBot: {
+    margin: '0px 30px',
+    marginBottom: '40px'
+  },
+  container: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  textField: {
+    marginLeft: theme.spacing.unit,
+    marginRight: theme.spacing.unit,
+    width: 400,
+  },
   bigAvatar: {
     width: 150,
     height: 150,
@@ -16,7 +35,7 @@ const styles = theme => ({
     ...theme.mixins.gutters(),
     paddingTop: theme.spacing.unit * 4,
     paddingBottom: theme.spacing.unit * 4,
-    margin: '50px 200px',
+    margin: '50px 170px',
     display: 'flex',
     flexDirection: 'row',
     alignItems: 'center',
@@ -26,31 +45,42 @@ const styles = theme => ({
     maxWidth: 500,
   },
   userData: {
-    margin: '0px 30px'
-  },
-  button: {
-    margin: '0px',
+    margin: '0px 30px',
   },
   icon: {
     fontSize: 24,
     padding: '0px',
     marginLeft: '10px',
   },
+  baseline: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'baseline',
+  }
 });
 
 const GitUser = (props) => {
   const { classes } = props;
   const call = (e) => {
     e.preventDefault()
-    props.actions.fetchProfile(e.target.children[0].value)
-    console.log(e.target.children[0].value)
+    props.actions.fetchProfile(e.target.children[0].children[1].children[0].value)
+    console.log(e.target.children[0].children[1].children[0].value)
   }
 
   return (
     <div>
-      <form onSubmit={call}>
-        <input type="text" defaultValue="13shutt"/>
-        <button>Find user!</button>
+      <form onSubmit={call} className={classes.container} autoComplete="off">
+        <TextField
+          name="username"
+          label="username"
+          defaultValue="13shutt"
+          className={classes.textField}
+          margin="normal"
+        />
+        <Button variant="outlined" color="primary" type="submit">
+          Find user!
+        </Button>
       </form>
       <Paper className={classes.root} elevation={1}>
         <Avatar
@@ -61,10 +91,32 @@ const GitUser = (props) => {
           <Typography variant="h5" gutterBottom>Login: {props.github.user.login}</Typography>
           <Typography variant="h5" gutterBottom>Name: {props.github.user.name}</Typography>
           <Typography variant="h5" gutterBottom>Location: {props.github.user.location}</Typography>
-          <Button href={props.github.user.html_url} variant="contained" color="primary" className={classes.button}>
+          <Button href={props.github.user.html_url} variant="contained" color="primary">
             github.com
             <Link className={classes.icon} />
           </Button>
+        </div>
+        <div className={classes.marginBot}>
+          <Typography variant="h6" gutterBottom  className={classes.baseline}>
+            Repos: {props.github.user.public_repos}
+            <Note className={classes.icon} />
+          </Typography>
+          <Typography variant="h6" gutterBottom  className={classes.baseline}>
+            Followers: {props.github.user.followers}
+            <People className={classes.icon} />
+          </Typography>
+          <Typography variant="h6" gutterBottom  className={classes.baseline}>
+            Following: {props.github.user.following}
+            <PeopleOutline className={classes.icon} />
+          </Typography>
+        </div>
+        <div className={classes.marginBot}>
+        <Typography variant="overline" gutterBottom>
+          Created: {props.github.user.created_at}
+        </Typography>
+        <Typography variant="overline" gutterBottom>
+          Updated: {props.github.user.updated_at}
+        </Typography>
         </div>
       </Paper>
     </div>
